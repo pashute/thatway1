929075dacf1988549ba617cb887f6a984e75293f
pashute <pashute@gmail.com>
2017-07-31 09:47:28 +0900

    Completely works and nice

--- www/noamdata.js
+++ www/noamdata.js
@@ -1,14 +1,22 @@
 // This is a JavaScript file
 
     var elevatorHtml = "You are near <i class='icon ion-ios-upload-outline'></i> Elevator hall B<br /><small>pointing south</small>";
-    var gotoInfo = [ { goingto: " Restaurants", ginfo: "Walk 40 steps ahead. Turn left into the south corridor and 30 steps to the restaurants" },
-                     { goingto: "Clothing shops", ginfo: "There are several clothing shops for men women and children along this corridor.<br />"
-                      + "In the real app we will give you a list of shops to choose from. " },
-                     { goingto: "Discount Bank", ginfo: "Discount bank is right behind you. The ATM machine is to your right, 10 steps away." },
-                     { goingto: "Supermarket", ginfo: "There is no supermarket in Big Fashion. Rami Levy is accross the road, "
-                      + "and Shupersol is in the old Big Betchemish mall" },
-                     { goingto: "Toilets", ginfo: "Walk 15 steps ahead then turn right and walk 20 steps. "
-                      + "The first three doors to your right are the rest-room for the disabled, "
+    var gotoInfo = [ { goingto: " Restaurants", ginfo: "Walk 40 steps ahead.<br />"
+                      + "Turn left into the south corridor<br />"
+                      + "and 30 steps to the restaurants" },
+                     { goingto: "Clothing shops", ginfo: "There are several clothing shops<br /> "
+                      + "for men women and children along this corridor.<br />"
+                      + "<i>In the real app we will give you a list of shops to choose from.<i>" },
+                     { goingto: "Discount Bank", ginfo: "Discount bank is right behind you.<br />"
+                      + "The ATM machine is to your right, 10 steps away.<br /><br/>"
+                      + "The east exit is just past the bank."},
+                     { goingto: "Supermarket", ginfo: "There is no supermarket in Big Fashion.<br />"
+                      + "Rami Levy is accross the road, <br />"
+                      + "and Shupersol is in the old Big-Betchemish mall" },
+                     { goingto: "Toilets", ginfo: "Walk 15 steps ahead<br />"
+                      + "then turn right and walk 20 steps.<br /> "
+                      + "The first three doors to your right are as follows:<br />"
+                      + "the rest-room for the disabled,<br /> "
                       + "the men's room and then the women's room." },
                      { goingto: "Exits", ginfo: "Turn back to exit the elevator hall then turn left 500 meters for the North exit<br />"
                       + "or turn right and 20 steps to the South exit. <br />"
